5330ea76cc43623f0af6440f234cdad3f6dc0810
pop <petarpopovic719@gmail.com>
2024-03-05 21:46:19 +0100

    push

--- src/app/components/photos.tsx
+++ src/app/components/photos.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, {useEffect, useRef, useState} from "react";
 import PhotoAlbum, { Photo } from "react-photo-album";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
@@ -12,7 +12,6 @@ import "yet-another-react-lightbox/plugins/thumbnails.css";
 
 import Image from "next/image";
 import type { RenderPhotoProps } from "react-photo-album";
-import ScrollComponent from "@/app/components/ScrollComponent";
 import Link from "next/link";
 export function Photos({
                            photos,
@@ -22,17 +21,36 @@ export function Photos({
     })[];
 }) {
     const [index, setIndex] = useState(-1);
+    const [show, setShow] = useState(true);
+    const prevScrollPos = useRef(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollPos = window.scrollY;
+            const isScrollingUp = prevScrollPos.current > currentScrollPos;
+
+            setShow(isScrollingUp);
+            prevScrollPos.current = currentScrollPos;
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
     return (
         <>
-            <div className={`flex w-full h-[10vh] text-white justify-center items-center appear`}>
-                <Link href="/">
-                    <button className="p-1 transition-all duration-100 ease-in-out rounded border-b-2
-        hover:bg-zinc-700 hover:border-b-4 hover:border-t-2 hover:border-white hover:border-l-2 hover:border-r-2
-        active:transform active:border-b-0 active:translate-y-0 text-xl">
-                        Početna
-                    </button>
-                </Link>
-            </div>
+            {show && (
+                <div className={`flex w-full h-[8vh] text-white justify-center items-center appear sticky 
+                top-0 z-50 color`}>
+                    <Link href="/">
+                        <button className="p-1 transition-all duration-100 ease-in-out rounded border-b-2
+              hover:bg-zinc-700 hover:border-b-4 hover:border-t-2 hover:border-white hover:border-l-2 hover:border-r-2
+              active:transform active:border-b-0 active:translate-y-0 text-xl">
+                            Početna
+                        </button>
+                    </Link>
+                </div>
+            )}
             <div className="p-3 appear album">
                 <PhotoAlbum
                     // @ts-ignore
